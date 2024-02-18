5997a7b9b451eb03f6f33c5f7af86971fed274d2
jpaulopereira <joaopaulo.ae@gmail.com>
2024-02-18 17:38:34 -0300

    adicao usuario

---
+++ src/main/java/med/voll/api/infra/exception/security/SecurityConfigurations.java
@@ -0,0 +1,28 @@
+package med.voll.api.infra.exception.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigurations {
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.csrf()
+//                .disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().build();
+//    }
+
+    //versão 3.1 do spring
+    @Bean //Sem essa anotação no método, o objeto SecurityFilterChain não será exposto como um bean para o Spring.
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
