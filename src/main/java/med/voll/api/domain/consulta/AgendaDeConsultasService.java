c0d47c945ed812e74bbabcd17685a12bf575be34
jpaulopereira <joaopaulo.ae@gmail.com>
2024-03-04 08:43:42 -0300

    adicao agenda de consultas

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultasService.java
@@ -0,0 +1,45 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.infra.exception.ValidacaoException;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultasService {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendar (DadosAgendamentoConsulta dados) {
+
+        if(!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente invormado não existe");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico invormado não existe");
+        }
+
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
+
+
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+       if(dados.idMedico() != null) {
+           return medicoRepository.getReferenceById(dados.idMedico());
+       }
+    }
+}
