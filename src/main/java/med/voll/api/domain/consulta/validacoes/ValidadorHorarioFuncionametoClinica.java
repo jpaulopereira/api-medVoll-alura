e706bd173f3689e0043a50f67d94a3d6b3c00d0b
jpaulopereira <joaopaulo.ae@gmail.com>
2024-03-06 08:56:15 -0300

    validador

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionametoClinica.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionametoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY); //devolve um boolean
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+        if (domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horário");
+        }
+
+    }
+}
